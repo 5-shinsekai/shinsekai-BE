bcb52208e797723e7ce5abb9cada8fc4a02f2038
jeonghwan <silverice1973@naver.com>
2025-03-17 09:48:38 +0900

    Initial commit with Spring Boot setup

---
+++ src/main/java/com/example/shinsekai/ShinsekaiApplication.java
@@ -0,0 +1,13 @@
+package com.example.shinsekai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShinsekaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShinsekaiApplication.class, args);
+	}
+
+}
