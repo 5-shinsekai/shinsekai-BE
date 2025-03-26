64fa8559e480b1a6a64797e189368da91a46ec17
LeeYeongin <youngin5757@gmail.com>
2025-03-26 14:04:09 +0900

    feat: 장바구니 Entity 추가
    - Cart 테이블을 위한 Entity 추가
    
    Resolves: #10

---
+++ src/main/java/com/example/shinsekai/cart/entity/Cart.java
@@ -0,0 +1,41 @@
+package com.example.shinsekai.cart.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cartPk;
+
+    @Column(nullable = false, length = 255)
+    private String memberUuid;
+
+    @Column(nullable = false)
+    private Long productOptionkey;
+
+    @Column(nullable = false, length = 255)
+    private String productCode;
+
+    @Column(nullable = false)
+    private int count;
+
+    @Column(nullable = false, length = 255)
+    private boolean checked;
+
+    @Column(nullable = false, length = 255)
+    private String symbolMessage;
+
+    @Column(nullable = false)
+    private boolean freezedChecked;
+
+    @Column(nullable = false, length = 255)
+    private LocalDateTime storageLimitDate;
+}
