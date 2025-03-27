201b7cc7117a38de6e29ada91e4b6c2294782828
bugling <baebuseung@gmail.com>
2025-03-27 14:58:47 +0900

    Feat: 회원관련 자바 파일 업로드 - Security관련 파일과 의존성이 강하여 해당 브랜치에 묶음 - 강의 내용을 옮겨 붙인 상황(차후 수정 예정) - #14

---
+++ src/main/java/com/example/shinsekai/member/infrastructure/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.example.shinsekai.member.infrastructure;
+
+import com.example.shinsekai.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
+    Optional<Member> findByMemberUuid(String uuid);
+    Optional<Member> findByLoginId(String loginId);
+}
---
+++ src/main/java/com/example/shinsekai/member/vo/MemberSignUpVo.java
@@ -0,0 +1,16 @@
+package com.example.shinsekai.member.vo;
+
+import lombok.Getter;
+
+@Getter
+public class MemberSignUpVo {
+
+
+    private String name;
+    private String email;
+    private String password;
+    private String phone;
+    private String birth;
+
+}
+
