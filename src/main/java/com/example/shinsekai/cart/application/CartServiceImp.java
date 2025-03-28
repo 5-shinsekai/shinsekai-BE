9df35f0534337b33d78e5a29a5c874f1ec3f644c
LeeYeongin <youngin5757@gmail.com>
2025-03-28 15:15:13 +0900

    Feat: Cart Service productCode 유효성 검사
    - existsByProductCode로 유효성 검사
    
    Resolves: #11

--- src/main/java/com/example/shinsekai/cart/application/CartServiceImp.java
+++ src/main/java/com/example/shinsekai/cart/application/CartServiceImp.java
@@ -5,6 +5,7 @@ import com.example.shinsekai.cart.infrastructure.CartRepository;
 import com.example.shinsekai.common.entity.BaseResponseStatus;
 import com.example.shinsekai.common.exception.BaseException;
 import com.example.shinsekai.product.infrastructure.ProductOptionListRepository;
+import com.example.shinsekai.product.infrastructure.ProductRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,10 +18,15 @@ public class CartServiceImp implements CartService{
 
     private final CartRepository cartRepository;
     private final ProductOptionListRepository productOptionListRepository;
+    private final ProductRepository productRepository;
 
     @Override
     @Transactional
     public void createCart(CartCreateRequestDto cartCreateRequestDto) {
+        if (!productRepository.existsByProductCode(cartCreateRequestDto.getProductCode())) {
+            throw new BaseException(BaseResponseStatus.NO_EXIST_PRODUCT);
+        }
+
         if (!productOptionListRepository.existsById(cartCreateRequestDto.getProductOptionListId())) {
             throw new BaseException(BaseResponseStatus.NO_EXIST_OPTION);
         }
