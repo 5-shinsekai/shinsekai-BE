35ffcc63f8e91baaf4b0626e23147e4e35b3fbef
JeongHwan Kim <silverice1973@naver.com>
2025-03-30 19:02:02 +0900

    FEAT : 상품 관련 API - 소프트 딜리트 추가 - 하드 딜리트 추가 - 상품 상태 변경 (HIDDEN Or SELLING) - 상품 전체 조회 수정
    Resolves : #5

--- src/main/java/com/example/shinsekai/product/infrastructure/ProductRepository.java
+++ src/main/java/com/example/shinsekai/product/infrastructure/ProductRepository.java
@@ -1,13 +1,25 @@
 package com.example.shinsekai.product.infrastructure;
 
 import com.example.shinsekai.product.entity.Product;
+import com.example.shinsekai.product.entity.ProductStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
     Optional<Product> findByProductCode(String productCode);
     boolean existsByProductCode(String productCode);
+
+    Optional<Product> findByProductCodeAndIsDeletedFalse(String productCode);
+
+    Optional<Product> findByProductCodeAndIsDeletedTrue(String productCode);
+
+    List<Product> findAllByIsDeletedFalse();
+
+    List<Product> findAllByIsDeletedFalseAndProductStatus(ProductStatus status);
+
+
 }
