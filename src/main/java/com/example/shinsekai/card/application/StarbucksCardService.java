b0a9934e7e5c2912c9c2da9f513ffb36a1de5437
sujin31 <dltnwls301@gmail.com>
2025-04-10 16:42:28 +0900

    Feat: 스타벅스 카드 결제 API
    - 스타벅스 카드 결제 추가
    - 잔액 차감
    
    Resolves:#74

--- src/main/java/com/example/shinsekai/card/application/StarbucksCardService.java
+++ src/main/java/com/example/shinsekai/card/application/StarbucksCardService.java
@@ -1,6 +1,8 @@
 package com.example.shinsekai.card.application;
 
+import com.example.shinsekai.card.dto.in.MemberStarbucksListDto;
 import com.example.shinsekai.card.dto.in.StarbucksCardRequestDto;
+import com.example.shinsekai.card.dto.in.UseStarbucksCardRequestDto;
 import com.example.shinsekai.card.dto.out.StarbucksCardResponseDto;
 import com.example.shinsekai.card.entity.StarbucksCard;
 
@@ -9,6 +11,7 @@ import java.util.List;
 public interface StarbucksCardService {
     List<StarbucksCardResponseDto> getActiveStarbucksCards(String memberUuid);
     void createStarbucksCard(StarbucksCardRequestDto starbucksCardRequestDto);
-    void deleteStarbucksCard(String StarbucksCardUuid, String memberUuid);
+    void deleteStarbucksCard(MemberStarbucksListDto memberStarbucksCardsListDto);
+    void useRemainAmount(UseStarbucksCardRequestDto useStarbucksCardRequestDto);
 }
 
