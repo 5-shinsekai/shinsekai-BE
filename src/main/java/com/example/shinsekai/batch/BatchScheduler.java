c38944759b046250de5a719bd3e964cf826015eb
Sujin31 <52381101+Sujin31@users.noreply.github.com>
2025-04-21 21:49:52 +0900

    Update BatchScheduler.java

--- src/main/java/com/example/shinsekai/batch/BatchScheduler.java
+++ src/main/java/com/example/shinsekai/batch/BatchScheduler.java
@@ -25,7 +25,7 @@ public class BatchScheduler {
 
 
 //    @Scheduled(cron = "0/10 * * * * *")
-    @Scheduled(cron = "0 0 16 * * *")
+    @Scheduled(cron = "0 55 21 * * *")
     public void runPurchaseDailyAggregationJob() {
         try {
             LocalDate aggregationDate = LocalDate.now().minusDays(1); // 집계할 날짜
